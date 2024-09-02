9d51ba1e1ab25bb51c95418acee0e03688152a3b
KreyDev <quackface2@gmail.com>
2024-09-02 12:13:57 -0400

    first working alpha yippee!!!

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
+using Mirror;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.EventSystems;
 
 public class GameManager : MonoBehaviour
 {
-    public InputActionAsset actions;
+    public GameObject[] toggleVisOnJoin;
+    public RawImage charge;
     public static GameManager Singleton {
         get {
             if (_singleton is null) {
@@ -16,10 +19,65 @@ public class GameManager : MonoBehaviour
         }
     }
     static GameManager _singleton;
+    static NetworkManager nm;
     public float sceneGravity = -9.81f;
+    public Image heathBar;
+    public TMPro.TMP_Text addrLabel;
+    public TMPro.TMP_Text streakLabel;
+    public TMPro.TMP_InputField addrBox;
+    public GameObject ultText;
 
+    int UILayer;
     void Start() {
         DontDestroyOnLoad(gameObject);
+        nm = GetComponent<NetworkManager>();
+        UILayer = LayerMask.NameToLayer("UI");
     }
-    
+
+    public void Suffer() {
+        if (addrBox.text != "") {
+            nm.StartClient();
+            addrLabel.text = "bartonite on " + nm.networkAddress;
+        } else {
+            nm.StartHost();
+            addrLabel.text = "hosting bartonite";
+        }
+        foreach (var obj in toggleVisOnJoin) {
+            obj.SetActive(!obj.activeSelf);
+        }
+    }
+
+    public void Quit() {
+        if (addrBox.text != "") {
+            nm.StopClient();
+        } else {
+            nm.StopHost();
+        }
+        foreach (var obj in toggleVisOnJoin) {
+            obj.SetActive(!obj.activeSelf);
+        }
+    }
+
+    //Returns 'true' if we touched or hovering on Unity UI element.
+    public bool IsPointerOverUIElement() {
+        List<RaycastResult> eventSystemRaycastResults = GetEventSystemRaycastResults();
+        for (int index = 0; index < eventSystemRaycastResults.Count; index++)
+        {
+            RaycastResult curRaysastResult = eventSystemRaycastResults[index];
+            if (curRaysastResult.gameObject.layer == UILayer)
+                return true;
+        }
+        return false;
+    }
+
+
+    //Gets all event system raycast results of current mouse or touch position.
+    static List<RaycastResult> GetEventSystemRaycastResults()
+    {
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = Input.mousePosition;
+        List<RaycastResult> raysastResults = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, raysastResults);
+        return raysastResults;
+    }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 using UnityEngine.InputSystem;
-using System.Transactions;
+using UnityEngine.EventSystems;
+
 
 public class Player : NetworkBehaviour {
     Vector2 mouseDelta;
@@ -12,11 +13,13 @@ public class Player : NetworkBehaviour {
     Vector2 rotation;
     GameManager gm;
     CharacterController ctrl;
-    public float speed = 1;
+    public float speed;
+    public float glideSpeed;
     Vector2 rawInputMovement;
     public float yPrev;
     public bool isGrounded;
     public float jumpSpeed;
+    public float reGlideSpeed;
     public float height;
     bool jumpGo;
     public Vector3 vel;
@@ -25,7 +28,16 @@ public class Player : NetworkBehaviour {
     Vector2 smoothInput;
     public bool flight;
     public GameObject mesh;
-
+    public Animator animator;
+    bool cancelanim;
+    public bool gliding = true;
+    [SyncVar] public int charge;
+    [SyncVar] public int streak;
+    bool paused;
+    public GameObject hat;
+    [SyncVar] public int health;
+    public int maxhealth;
+    Collider prevAtk;
     void Start() {
         doGrav = false;
         cam = GetComponentInChildren<Camera>().gameObject;
@@ -40,7 +52,6 @@ public class Player : NetworkBehaviour {
             Cursor.visible = false;
         } else {
             cam.SetActive(false);
-            
         }
         StartCoroutine(GravTimer());
     }
@@ -62,6 +73,16 @@ public class Player : NetworkBehaviour {
                 rotation.x = Mathf.Clamp(rotation.x, -90, 90);
                 transform.localEulerAngles = new Vector3(0, rotation.y, 0);
                 cam.transform.localEulerAngles = new Vector3(rotation.x, 0, 0);
+                if (Input.GetKey(KeyCode.Escape)) {
+                    Cursor.lockState = CursorLockMode.None;
+                    Cursor.visible = true;
+                    paused = true;
+                }
+                if (Input.anyKey && !Input.GetKey(KeyCode.Escape) && !gm.IsPointerOverUIElement()) {
+                    Cursor.lockState = CursorLockMode.Locked;
+                    Cursor.visible = false;
+                    paused = false;
+                }
             }
 
             // gravity
@@ -72,8 +93,12 @@ public class Player : NetworkBehaviour {
             if (Vector3.Distance(smoothInput,rawInputMovement) >= .2)
                 smoothInput = Vector2.Lerp(smoothInput, rawInputMovement, 5f * Time.deltaTime);
             else smoothInput = rawInputMovement;
-            Vector3 vel = transform.forward * smoothInput.y * speed + transform.right * smoothInput.x * speed + new Vector3(0, yVel, 0);
-            
+            Vector3 vel;
+            if (gliding) vel = transform.forward * smoothInput.y * glideSpeed + transform.right * smoothInput.x * glideSpeed + new Vector3(0, yVel, 0);
+            else vel = transform.forward * smoothInput.y * speed + transform.right * smoothInput.x * speed + new Vector3(0, yVel, 0);
+            if (smoothInput.magnitude >= .2f && !cancelanim) animator.SetInteger("state", 1);
+            else if (!cancelanim) animator.SetInteger("state", 0);
+
             ctrl.Move(vel * Time.deltaTime);
         }
         
@@ -90,6 +115,7 @@ public class Player : NetworkBehaviour {
             layerMask = ~layerMask;
             if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, height, layerMask)) {
                 isGrounded = true;
+                gliding = false;
                 // print("jump cast hit");
             } else {
                 isGrounded = false;
@@ -102,6 +128,20 @@ public class Player : NetworkBehaviour {
             }
         } else {
             if (jumpGo) yVel = Mathf.Lerp(yVel, jumpSpeed, 0.1f);
+        }
+        if (isLocalPlayer) {
+            // update ui
+            if (charge < 5) {
+                gm.charge.texture = Resources.Load<Texture>("charge-" + charge);
+                gm.ultText.SetActive(false);
+            } else { 
+                gm.charge.texture = Resources.Load<Texture>("charge-" + 5);
+                gm.ultText.SetActive(true);
+            }
+
+            gm.heathBar.fillAmount = (float)health / maxhealth;
+
+            gm.streakLabel.text = "streak: " + streak;
         }
     }
 
@@ -119,7 +159,89 @@ public class Player : NetworkBehaviour {
 
     public void OnJump(InputAction.CallbackContext value){
         // print(value.ReadValue<float>());
-        if (value.ReadValue<float>() == 0) jumpGo = false;
+        if (value.ReadValue<float>() == 0 && isLocalPlayer) jumpGo = false;
         else jumpGo = true;
+    }
+
+    public void OnAtkPrim(InputAction.CallbackContext value){
+        if (value.ReadValueAsButton() && !cancelanim && isLocalPlayer) {
+            StartCoroutine(IAtkPrim());
+        }
+    }
+
+    IEnumerator IAtkPrim() {
+        animator.SetInteger("state", 2);
+        cancelanim = true;
+        hat.layer = LayerMask.NameToLayer("Default");
+        yield return new WaitForSecondsRealtime(1.8f);
+        cancelanim = false;
+        hat.layer = LayerMask.NameToLayer("LocalPlayer");
+    }
+
+    public void OnAtkSec(InputAction.CallbackContext value){
+        if (value.ReadValueAsButton() && isLocalPlayer) {   
+            StartCoroutine(IAtkPrim());
+        }
+    }
+
+    public void OnReGlide(InputAction.CallbackContext value){
+        if (value.ReadValueAsButton() && charge == 5 && isLocalPlayer) {
+            StartCoroutine(IReGlide());
+        }
+    }
+
+    IEnumerator IReGlide() {
+        // split server and client side of this to prevent charge lock after reglide
+        yVel = reGlideSpeed;
+        for (int i = 0; i < 5; i++) {
+            charge--;
+            yield return new WaitForSecondsRealtime(.2f);
+        }
+        gliding = true;
+        yVel = 0;
+    }
+
+    void OnTriggerEnter(Collider col) {
+        StartCoroutine(IHatCol(col));
+    }
+
+    IEnumerator IHatCol(Collider col) {
+        if (prevAtk != col && col.GetComponentInParent<Player>() != this) {
+            prevAtk = col;
+            if (col.gameObject.name == "hat")
+                CmdDmg(1, col.GetComponentInParent<Player>());
+            yield return new WaitForSecondsRealtime(.8f);
+            prevAtk = null;
+        }
+    }
+
+    IEnumerator _Die() {
+        cancelanim = true;
+        animator.SetInteger("state", 4);
+        yVel = reGlideSpeed;
+        for (int i = 0; i < 6; i++) {
+            yVel = reGlideSpeed / i;
+            yield return new WaitForSecondsRealtime(.2f);
+        }
+        cancelanim = false;
+        gliding = true;
+        yVel = 0;
+    }
+
+    [ClientRpc] void Die() {
+            StartCoroutine(_Die());
+    }
+
+    [Command] void CmdDmg(int amount, Player origin) {
+        health -= amount;
+        if (health == 0) {
+            if (origin.charge < 5)
+                origin.charge++;
+            origin.streak++;
+            streak = 0;
+            charge = 0;
+            health = maxhealth;
+            Die();
+        }
     }
 }
